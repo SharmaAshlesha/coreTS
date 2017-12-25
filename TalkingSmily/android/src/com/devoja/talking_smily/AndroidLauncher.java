8f2867842f5832c730fcd9fdb83790da2a9acaed
Ashlesha Sharma <sharmaashlesha90@gmail.com>
2017-12-25 17:32:28 +0530

    First commit

---
+++ TalkingSmily/android/src/com/devoja/talking_smily/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.devoja.talking_smily;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.devoja.talking_smily.TalkingSmily;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TalkingSmily(), config);
+	}
+}
---
+++ TalkingSmily/desktop/src/com/devoja/talking_smily/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.devoja.talking_smily.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.devoja.talking_smily.TalkingSmily;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TalkingSmily(), config);
+	}
+}
